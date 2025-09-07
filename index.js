93efcbc7caabd237af09696f9b35e24bdaf855ab
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-09-07 12:17:41 +0530

    cors policy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.set("trust proxy", true);
 
